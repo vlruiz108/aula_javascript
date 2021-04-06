85941c56d5c583f6af0cd565d291d1c595e39c44
vlruiz108 <vlruiz@hotmail.com>
2021-04-06 23:20:55 +0200

    treinando variaveis

--- aula_javascript/js/main.js
+++ aula_javascript/js/main.js
@@ -1,4 +1,26 @@
+var d = new Date();
+alert(d);
 
+/*
+var count = 0;
+while(count <= 5);
+console.log(count);
+count = count + 1;
+*/
+
+/*
+var idade = prompt("Qual é a sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/*
+var fruta = {nome: "morango", cor:"vermelho"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
 
 //var nome = "Van Ruiz";
 //var idade = 18;
@@ -7,3 +29,9 @@
 //console.log(idade);
 //console.log(frase);
 //alert(frase.toLowerCase());
+//var lista = ["maça ", " pêra ", " laranja"];
+//lista.pop();
+//lista.push("kiwi");
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
