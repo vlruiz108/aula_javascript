95cbce244c50d27226195af725418d0397c58f11
vlruiz108 <vlruiz@hotmail.com>
2021-04-06 19:56:26 +0200

    atualizacao das pastas

---
+++ aula_javascript/js/main.js
@@ -0,0 +1,9 @@
+
+
+//var nome = "Van Ruiz";
+//var idade = 18;
+//var frase = " A comida espanhola é a melhor do mundo ";
+//console.log(nome);
+//console.log(idade);
+//console.log(frase);
+//alert(frase.toLowerCase());
